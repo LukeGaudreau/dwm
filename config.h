b8f1a4a72271c4c1fe516b2f5446609705f7e051
Luke Gaudreau <luke@infosoph.org>
2017-03-20 14:48:33 -0400

    Add basic workspace config

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "base", "web", "code", "gtd", "vm", "office", "art", "play", "test" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -38,7 +38,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Google-chrome",  NULL, NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
