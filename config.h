3c28605a96a26996c43ee6e4f1852d0200d6db78
Luke Gaudreau <gaudreal@bc.edu>
2017-09-19 15:05:01 -0400

    Switch to st

--- config.h
+++ config.h
@@ -78,7 +78,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-p", "run:", "-m", dmenumon, "-fn", dmenufont, "-nb", colors[0][2], "-nf", colors[0][1], "-sb", colors[1][2], "-sf", colors[1][1], NULL };
 static const char *passcmd[] = { "passmenu", "-p", "pass:", "-m", dmenumon, "-fn", dmenufont, "-nb", colors[0][2], "-nf", colors[0][1], "-sb", colors[1][2], "-sf", colors[1][1], NULL };
-static const char *termcmd[]  = { "xterm", NULL };
+static const char *termcmd[]  = { "st", NULL };
 static const char *mailcmd[]  = { "termite", "-e", "mutt", "-t", "mutt", NULL };
 
 static Key keys[] = {
