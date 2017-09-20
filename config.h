4d0444d5aace11c67e39836cc13727b3146662a2
Luke Gaudreau <gaudreal@bc.edu>
2017-09-20 09:44:37 -0400

    Switch font to Ubuntu Mono
    Ubuntu was causing problems with dwm-status

--- config.h
+++ config.h
@@ -4,10 +4,10 @@
 
 /* appearance */
 static const char *fonts[] = {
-	"Ubuntu:size=11",
-	"FontAwesome:size=12"
+	"Ubuntu Mono:size=12",
+	"FontAwesome:size=11"
 };
-static const char dmenufont[]       = "Ubuntu:size=11";
+static const char dmenufont[]       = "Ubuntu Mono:size=12";
 #define NUMCOLORS         9
 static const char colors[NUMCOLORS][MAXCOLORS][9] = {
   // border   foreground background
