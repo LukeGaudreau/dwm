801f4e936db02ae58bfd0c3882f6f7307b1c992c
Luke Gaudreau <gaudreal@bc.edu>
2018-12-21 11:35:48 -0500

    Keybinding updates

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -61,13 +63,22 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray3, NULL };
 static const char *pmenucmd[] = { "passmenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray3, NULL };
+static const char *displaycmd[] = { "displaymenu", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray4, "-sf", col_gray3, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = pmenucmd } },
+	{ MODKEY|ControlMask,           XK_p,      spawn,          SHCMD("dock") },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+        { 0,             XF86XK_MonBrightnessDown, spawn,          SHCMD("brightnessctl -q s 10%-") },
+        { 0,             XF86XK_MonBrightnessUp,   spawn,          SHCMD("brightnessctl -q s +10%") },
+        { ShiftMask,     XF86XK_MonBrightnessUp,   spawn,          SHCMD("brightnessctl -q s 100%") },
+	{ 0,             XF86XK_AudioLowerVolume,  spawn,          SHCMD("volume down") },
+	{ 0,             XF86XK_AudioMute,         spawn,          SHCMD("volume toggle") },
+	{ 0,             XF86XK_AudioRaiseVolume,  spawn,          SHCMD("volume up") },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,	   SHCMD("slock") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -85,10 +96,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_Left,   focusmon,       {.i = -1 } },
+        { MODKEY,                       XK_Right,  focusmon,       {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_Left,   tagmon,         {.i = -1 } },
+        { MODKEY|ShiftMask,             XK_Right,  tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
