29f266bcd56db3c685ec1997bb90ef47b75554b4
Luke Gaudreau <gaudreal@bc.edu>
2018-12-21 11:35:08 -0500

    use sans

--- config.h
+++ config.h
@@ -6,9 +6,9 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 10;       /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12",
+static const char *fonts[]          = { "sans:size=11",
 					"icomoon:size=11"};
-static const char dmenufont[]       = "monospace:size=12";
+static const char dmenufont[]       = "sans:size=11";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#e3e0d5";
